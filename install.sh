d378e6caed6729424b8885a3883a1bac684a72bf
starvingarden <91560218+starvingarden@users.noreply.github.com>
2024-01-27 14:30:05 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
+#######
+# notes
+#######
+
+# each disk has its own physical volume, volume group, and logical volume(s). RAID is used via btrfs
+# btrfs does NOT support having different raid levels in the same filesystem
+# btrfs raid1 supports 2 or more disks, 50% of total storage utilization
+# to create a non-raid filesystem across multiple drives (mkfs.btrfs -L filesystemLabel -f -m dup -d single /dev/sda1 /dev/sdb1 /dev/sdc1)
+# to create a raid filesystem (mkfs.btrfs -L filesystemLabel -f -m raid1 -d raid1 /dev/sda1 /dev/sdb1 /dev/sdc1)
+# to add a disk sdb1 to a single disk filesystem on sda1 and convert to raid1 (mount /dev/sda1 /mnt && btrfs device add /dev/sdb1 /mnt && btrfs balance start -dconvert=raid1 -mconvert=raid1 /mnt)
+
+
+
 ############
 # need to do
 ############
 
 # see arch wiki page "btrfs#Multi-device file system" for information on how to convert raid levels, and add/remove/replace devices
 # get disk serial number with lsblk -o name,serial (user should use this to physically label cable/disks osdisk1, etc.)
-# each disk has its own physical volume, volume group, and logical volume(s). RAID is used via btrfs
 # user should specify disks in the order they want (osdisk1 should be the 1st listed, osdisk2 2nd listed, etc.)
 # kpartx command to use disks that are already configured
 # use persistent block device naming for initramfs and grub configuration
 # configure script to work if there are no data disks
 # change rootPaths and dataPaths to rootlvPaths and datalvPaths???
-
-# btrfs does NOT support having different raid levels in the same filesystem
-# btrfs raid1 supports 2 or more disks, 50% of total storage utilization
-# to create a non-raid filesystem across multiple drives (mkfs.btrfs -L filesystemLabel -f -m dup -d single /dev/sda1 /dev/sdb1 /dev/sdc1)
-# to create a raid filesystem (mkfs.btrfs -L filesystemLabel -f -m raid1 -d raid1 /dev/sda1 /dev/sdb1 /dev/sdc1)
-# to add a disk sdb1 to a single disk filesystem on sda1 and convert to raid1 (mount /dev/sda1 /mnt && btrfs device add /dev/sdb1 /mnt && btrfs balance start -dconvert=raid1 -mconvert=raid1 /mnt)
 
 
 
