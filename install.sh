daf4a8807f5262a35258d8adecb961def2f74b76
starvingarden <91560218+starvingarden@users.noreply.github.com>
2024-12-20 10:36:10 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,9 @@
 # to create a non-raid filesystem across multiple drives (mkfs.btrfs -L filesystemLabel -f -m dup -d single /dev/sda1 /dev/sdb1 /dev/sdc1)
 # to create a raid filesystem (mkfs.btrfs -L filesystemLabel -f -m raid1 -d raid1 /dev/sda1 /dev/sdb1 /dev/sdc1)
 # to add a disk sdb1 to a single disk filesystem on sda1 and convert to raid1 (mount /dev/sda1 /mnt && btrfs device add /dev/sdb1 /mnt && btrfs balance start -dconvert=raid1 -mconvert=raid1 /mnt)
+# see arch wiki page "btrfs#Multi-device file system" for information on how to convert raid levels, and add/remove/replace devices
+# user should specify disks in the order they want (osdisk0 should be the 1st listed, osdisk1 2nd listed, etc.)
+# kpartx command to use disks that are already configured
 
 
 
@@ -18,18 +21,12 @@
 # need to do
 ############
 
-# see arch wiki page "btrfs#Multi-device file system" for information on how to convert raid levels, and add/remove/replace devices
 # get disk serial number with lsblk -o name,serial (user should use this to physically label cable/disks osdisk0, etc.)
-# user should specify disks in the order they want (osdisk0 should be the 1st listed, osdisk1 2nd listed, etc.)
-# kpartx command to use disks that are already configured
 # use persistent block device naming for initramfs and grub configuration
 # run a script every so often to check battery level and charging status and hibernate if necessary
 # make a "variables" file for user to manually enter variables into, instead of editing the install.sh file, edit the variables.txt file
-# make a script/git repo for system files similar to scripts
 # add scripts to PATH in /etc/profile
 # change directory name /.snapshots/backups to /.snapshots/tmp??
-# make systemd service that hibernates on low battery
-
 
 
 
